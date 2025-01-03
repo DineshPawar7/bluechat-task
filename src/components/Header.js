d471d1c75899be1707d8ea8537d9d8ae631f365a
Dinesh Pawar <dsp70458@gmail.com>
2025-01-03 14:42:58 +0530

    Add sidebar in project

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,64 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "../styles/Header.css";
-import { AiOutlineArrowRight, AiOutlineArrowLeft, AiOutlineLock, AiOutlinePlus, AiOutlineUpload } from 'react-icons/ai';
+import {
+  AiOutlineArrowRight,
+  AiOutlineArrowLeft,
+  AiOutlineLock,
+  AiOutlinePlus,
+  AiOutlineUpload,
+} from "react-icons/ai";
 import { IoIosLink } from "react-icons/io";
 import { ImSpinner11 } from "react-icons/im";
+import { TbLayoutSidebar } from "react-icons/tb";
 
 
 const Header = () => {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const timer = setTimeout(() => setLoading(false), 2000);
-        return () => clearTimeout(timer);
-    }, []);
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
-        <header className='header'>
-            <div className='left-section'>
-                {loading ? (
-                    <>
-                    <div className='skeleton'></div>
-                    <div className='skeleton'></div>
-                    <div className='skeleton'></div>
-                    </>
-                ) : (
-                    <>
+  return (
+    <header className="header">
+      <div className="left-section">
+        
+          <div className="skeleton-1"></div>
+          <div className="skeleton-1"></div>
+          <div className="skeleton-1"></div>
 
-                    <AiOutlineArrowLeft className="icon" />
-                    <AiOutlineArrowRight className="icon" />
-                    <ImSpinner11 className="icon" />
-                    </>
-                )}
-            </div>
+          <div className="header-sidebar-icon"><TbLayoutSidebar /></div>
+        
+        {loading ? (
+          <>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+            <div className="skeleton"></div>
+          </>
+        ) : (
+          <>
+            <AiOutlineArrowLeft className="icon" />
+            <AiOutlineArrowRight className="icon" />
+            <ImSpinner11 className="icon" />
+          </>
+        )}
+      </div>
 
-            <div className='center-section'>
-                <AiOutlineLock className="icon-center" />
-                <a href='https://bluereceipt.com' className='link'>bluereceipt.com</a>
-                <IoIosLink className="icon-center" />
-            </div>
+      <div className="center-section">
+        <AiOutlineLock className="icon-center" />
+        <a href="https://bluereceipt.com" className="link">
+          bluereceipt.com
+        </a>
+        <IoIosLink className="icon-center" />
+      </div>
 
-            <div className='right-section'>
-                <AiOutlinePlus className="icon"/>
-                <AiOutlineUpload className="icon" />
-            </div>
-
-
-        </header>
-    )
-}
+      <div className="right-section">
+        <AiOutlinePlus className="icon" />
+        <AiOutlineUpload className="icon" />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
